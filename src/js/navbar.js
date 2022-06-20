79d9f516ff8c859cc4b588c8f2d77f82b23f4d9e
Skorczan <skorek11@gmail.com>
2022-06-20 21:01:08 +0200

    added simple navbar
    with scroll status

---
+++ src/js/navbar.js
@@ -0,0 +1,6 @@
+const bar = document.getElementById("status")
+const statusBar = () => {
+  currentScroll = Math.round(window.scrollY / (document.documentElement.offsetHeight - window.innerHeight) * 100)
+  bar.style.width = currentScroll + "%"
+}
+window.addEventListener("scroll", statusBar)
